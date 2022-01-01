ea3c58178ac67eaf46b510a815f4906658eb8179
paopaolopaoloo <simonruzzel@gmail.com>
2022-01-01 19:47:26 +0800

    js in applicant registration

---
+++ admission/applicant/js/applicant-reg.js
@@ -0,0 +1,66 @@
+function register() {
+    // Get all inputs
+    const APPLICANT_REG_INFO = {
+        firstName : document.forms["reg-form"]["f-name"].value,
+        middleName : document.forms["reg-form"]["m-name"].value,
+        lastName : document.forms["reg-form"]["l-name"].value,
+        suffix : document.forms["reg-form"]["suffix"].value,
+        birthDate : document.forms["reg-form"]["birthdate"].value,
+        sex : getSex(),
+        status : getStatus(),
+        course : document.forms["reg-form"]["course"].value,
+        strand : document.forms["reg-form"]["strand"].value,
+        email : document.forms["reg-form"]["email"].value,
+        password : matchPassword()
+    };
+
+    console.log(APPLICANT_REG_INFO)
+}
+
+function matchPassword() {
+    let a = document.getElementById("pass").value;
+    let b = document.getElementById("confirm-pass").value;
+
+    if (a != b) {
+        alert("Password not matched.");
+    }
+
+    else {
+        return a;
+    }
+}
+
+function getSex() {
+    let sex = undefined;
+
+    if ( document.getElementById("sex-male").checked ) {
+        sex = "Male";
+    }
+    else if ( document.getElementById("sex-female").checked ) {
+        sex = "Female";
+    }
+    
+    return sex;
+}
+
+function getStatus() {
+    let status = undefined;
+
+    if ( document.getElementById("freshmen").checked ) {
+        status = "Freshmen";
+    }
+    else if ( document.getElementById("transferee").checked ) {
+        status = "Transferee";
+    }
+    else if ( document.getElementById("returnee").checked ) {
+        status = "Returnee";
+    }
+    else if ( document.getElementById("cross-enrollee").checked ) {
+        status = "Cross-Enrolee";
+    }
+    else if ( document.getElementById("als").checked ) {
+        status = " ALS";
+    }
+
+    return status;
+} 
