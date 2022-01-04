1173834989d6f16ead8ea754c2b970c5febf7682
cefbok <xcef.1991@gmail.com>
2022-01-04 19:04:39 +0800

    nurse js

--- admission/medical/js/nurse-table.js
+++ admission/medical/js/nurse-table.js
@@ -1,18 +1,19 @@
+var rowdata = undefined;
 //JS FOR NURSE TABLE
 
 //Student Info----
 let studentData = [
   {radio:'<input class="radio" type="radio" name="flexRadio" id="flexRadio"></input>', 
-  name:'Frencillo, Paolo', number:'0165', course:'BET-COET', email:'pao@gmail.com', result:'', 
+  name:'Frencillo, Paolo', number:'0165', course:'BET-COET', email:'pao@gmail.com', mresult:'', 
   file:'<input type="file" name="image" id="image"></input>'},
   {radio:'<input class="radio" type="radio" name="flexRadio" id="flexRadio"></input>', 
-  name:'Paanod, Cefrin', number:'0185', course:'BET-ESET', email:'cef@gmail.com', result:'', 
+  name:'Paanod, Cefrin', number:'0185', course:'BET-ESET', email:'cef@gmail.com', mresult:'', 
   file:'<input type="file" name="image" id="image"></input>'},
   {radio:'<input class="radio" type="radio" name="flexRadio" id="flexRadio"></input>', 
-  name:'Montaril, Vincent Jake', number:'0175', course:'BET-PPET', email:'monta@gmail.com', result:'', 
+  name:'Montaril, Vincent Jake', number:'0175', course:'BET-PPET', email:'monta@gmail.com', mresult:'', 
   file:'<input type="file" name="image" id="image"></input>'},
   {radio:'<input class="radio" type="radio" name="flexRadio" id="flexRadio"></input>', 
-  name:'Kilario, Roniel', number:'0195', course:'BS Civil Engineering', email:'kilario@gmail.com', result:'', 
+  name:'Kilario, Roniel', number:'0195', course:'BS Civil Engineering', email:'kilario@gmail.com', mresult:'', 
   file:'<input type="file" name="image" id="image"></input>'}
 ];
 
@@ -26,11 +27,11 @@ function loadTableData(studentData) {
   let dataHtml = '';
 
   for(let student of studentData) {
-    dataHtml += `<tr><td>${student.radio}</td><td>${student.name}</td><td>${student.number}</td>
-    <td>${student.course}</td><td>${student.email}</td><td>${student.result}</td><td>${student.file}</td></tr>`;
+    dataHtml += `<tr id="${student.number}" onclick="selectedrow(event)"><td>${student.radio}</td><td>${student.name}</td><td>${student.number}</td>
+    <td>${student.course}</td><td>${student.email}</td><td class="row-result">${student.mresult}</td><td>${student.file}</td></tr>`;
   }
-  tableBody.innerHTML = dataHtml
 
+  tableBody.innerHTML = dataHtml
 }
 
 //radio check
@@ -38,18 +39,35 @@ $('#mytable tr').click(function() {
   $(this).find('td input:radio').prop('checked', true);
 })
 
+function selectedrow(event) {
+    var rowId = event.target.parentNode.id;
+  //this gives id of tr whose button was clicked
+    var data = document.getElementById(rowId).querySelectorAll(".row-result"); 
+  /*returns array of all elements with 
+  "row-data" class within the row with given id*/
+    console.log(rowId);
+    rowdata = rowId;
+}
+
+
 //pass-fail function
 function clickedPassed() {
-  /* BLOCK OF CODE HERE WHEN RADIO BUTTON IS CHECKED
-  AND PASSED BUTTON IS CLICKED */
-  console.log("Yamete");
+  var data = document.getElementById(rowdata).querySelectorAll(".row-result"); 
+  data[0].innerHTML = "PASSED"
+  // var inputText = document.getElementsByName('Passed').value;
 
+  // studentData.push(inputText);
+  // var pval = "";
+  // for(i = 0; i < studentData.length; i++) {
+  //   pval = pval + studentData[i];
+  // }
+
+  // document.getElementById('tabledata').innerHTML = pval;
 }
 
 function clickedFailed() {
-  /* BLOCK OF CODE HERE WHEN RADIO BUTTON IS CHECKED
-  AND FAILED BUTTON IS CLICKED */
-  console.log("Kudasai");
+  var data = document.getElementById(rowdata).querySelectorAll(".row-result"); 
+  data[0].innerHTML = "FAILED"
 
 }
 
