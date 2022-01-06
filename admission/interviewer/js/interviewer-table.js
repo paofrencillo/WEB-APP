cdb414911f58a1b795f3aaaaaa693ab457bdbde9
VincentJakeMontaril <vincentjake.montaril@gsfe.tupcavite.edu.ph>
2022-01-06 19:10:53 +0800

    js interviewer table

--- admission/interviewer/js/interviewer-table.js
+++ admission/interviewer/js/interviewer-table.js
@@ -1,28 +1,32 @@
-var rowdata = undefined;
-var a = undefined;
 
+
+var rowId = undefined;
+var highlightedRow = undefined;
 //Student Info
-let studentData = [
-  {cb:'<input type="checkbox" id="int-cb" name="cb" value="int-cb">',
-  name:'Montaril, Vincent Jake', number:'190187', course:'BET-COET', email:'monta@gmail.com', date:'11/06/2021', 
-  venue:'Industrial Department', interviewer:'Ms.Dela cruz', mresult:'',},
-  {cb:'<input type="checkbox" id="int-cb" name="cb" value="int-cb">',
-  name:'Frencillo, Paolo', number:'190666', course:'BET-COET', email:'pao@gmail.com', date:'11/06/2021', 
-  venue:'Industrial Department', interviewer:'Ms.Dela cruz', mresult:'',},
-  {cb:'<input type="checkbox" id="int-cb" name="cb" value="int-cb">',
-  name:'Paanod, Cefrin', number:'190123', course:'BET-ESET', email:'cef@gmail.com', date:'11/10/2021', 
-  venue:'Vulcanizing Department', interviewer:'Ms.Nalalabuan', mresult:'',},
-  {cb:'<input type="checkbox" id="int-cb" name="cb" value="int-cb">',
-  name:'Kilario, Roniel', number:'190321', course:'BET-PPET', email:'ron@gmail.com', date:'11/15/2021', 
-  venue:'TBA', interviewer:'Mr.Johnny Simp', mresult:'',},
-  {cb:'<input type="checkbox" id="int-cb" name="cb" value="int-cb">',
-  name:'Curry, Steph', number:'190030', course:'BSCE', email:'step@gmail.com', date:'11/20/2021', 
-  venue:'Engineering Department', interviewer:'Ms.Sana', mresult:'',},
-  {cb:'<input type="checkbox" id="int-cb" name="cb" value="int-cb">',
-  name:'James, Lebron', number:'190623', course:'BSCE', email:'bron@gmail.com', date:'11/20/2021', 
-  venue:'Engineering Department', interviewer:'Ms.Sana', mresult:'',},
-  
+var studentData = [
+  {edit:'<i class="bi bi-pencil-square" onclick="editFunction(event)" ></i>',
+  name:'Montaril, Vincent Jake', number:'190187', course:'BET-COET', email:'monta@gmail.com', date:'<input type="date" id="190187" disabled>' , 
+  venue:'Industrial Department', interviewer:'Ms.Dela cruz', result:'',},
+  {edit:'<i class="bi bi-pencil-square" onclick="editFunction(event)" ></i>',
+  name:'Frencillo, Paolo', number:'190666', course:'BET-COET', email:'pao@gmail.com', date:'<input type="date" id="190666" disabled>', 
+  venue:'Industrial Department', interviewer:'Ms.Dela cruz', result:'',},
+  {edit:'<i class="bi bi-pencil-square" onclick="editFunction(event)" ></i>',
+  name:'Paanod, Cefrin', number:'190123', course:'BET-ESET', email:'cef@gmail.com', date:'<input type="date" id="190123" disabled>',
+  venue:'Vulcanizing Department', interviewer:'Ms.Nalalabuan', result:'',}, 
+  {edit:'<i class="bi bi-pencil-square" onclick="editFunction(event)" ></i>',
+  name:'Kilario, Roniel', number:'190321', course:'BET-PPET', email:'ron@gmail.com', date:'<input type="date" id="190321" disabled>', 
+  venue:'TBA', interviewer:'Mr.Johnny Simp', result:'',},
+  {edit:'<i class="bi bi-pencil-square" onclick="editFunction(event)" ></i>',
+  name:'Curry, Steph', number:'190030', course:'BSCE', email:'step@gmail.com', date:'<input type="date" id="190030" disabled>', 
+  venue:'Engineering Department', interviewer:'Ms.Sana', result:'',},
+  {edit:'<i class="bi bi-pencil-square" onclick="editFunction(event)" ></i>',
+  name:'James, Lebron', number:'190623', course:'BSCE', email:'bron@gmail.com', date:'<input type="date" id="190623" disabled>', 
+  venue:'Engineering Department', interviewer:'Ms.Sana', result:'',},
 ];
+
+var selectedRowData = [
+  {date:'', venue:'', interviewer:'', result:'' }
+]
 
 loadTableData(studentData)
 
@@ -32,8 +36,8 @@ function loadTableData(studentData) {
   let dataHtml = '';
 
   for(let student of studentData) {
-    dataHtml += `<tr course = "${student.course}" id="${student.number}" onclick="selectedrow(event)">
-                  <td>${student.cb}</td>  
+    dataHtml += `<tr course = "${student.course}" id="${student.number}">
+                  <td>${student.edit}</td>  
                   <td>${student.name}</td>
                   <td>${student.number}</td>
                   <td>${student.course}</td>
@@ -41,76 +45,143 @@ function loadTableData(studentData) {
                   <td>${student.date}</td>
                   <td>${student.venue}</td>
                   <td>${student.interviewer}</td>
-                  <td class="row-result">${student.mresult}</td>
+                  <td class="row-result">${student.result}</td>
                 </tr>`;
   }
 
   tableBody.innerHTML = dataHtml
 }
 
-//row selected
-highlight_row();
-function highlight_row() {
-    var table = document.getElementById('mytable');
-    var cells = table.getElementsByTagName('td');
+function editFunction(event){
+  var rowdata = event.target.parentNode.parentNode.id;
+  if (event.target) {
+    rowId = rowdata;
+    var rowSelected = document.getElementById(rowId);
+    console.log("rowId", rowId);
+    rowSelected.style.backgroundColor = "rgb(189, 32, 49)";
+    rowSelected.style.color = "rgb(255, 255, 255)";
+    var data = document.getElementById(rowId).querySelectorAll("td");
+    console.log(data);
+    var date = data[5].firstElementChild.value;
+    var venue = data[6].innerHTML;
+    var interviewer = data[7].innerHTML;
+    var result = data[8].innerHTML;
+    
+    selectedRowData.date = date;
+    selectedRowData.venue = venue;
+    selectedRowData.interviewer = interviewer;
+    selectedRowData.result = result;
+    console.log(selectedRowData);
 
-    for (var i = 0; i < cells.length; i++) {
-        // Take each cell
-        var cell = cells[i];
-        // do something on onclick event for cell
-        cell.onclick = function () {
-            // Get the row id where the cell exists
-            var rowId = this.parentNode.rowIndex;
+    data[0].innerHTML='<i class="bi bi-save" onclick="saveFunction(event)" ></i> <span> <i class="bi bi-x" onclick="cancelFunction(event)"></i> </span>';
 
-            var rowsNotSelected = table.getElementsByTagName('tr');
-            for (var row = 0; row < rowsNotSelected.length; row++) {
-                rowsNotSelected[row].style.backgroundColor = "";
-                rowsNotSelected[row].classList.remove('selected');
-            }
-            var rowSelected = table.getElementsByTagName('tr')[rowId];
-           // rowSelected.style.backgroundColor = "rgb(189, 32, 49)";
-            rowSelected.className += " selected";
+    if (highlightedRow != undefined) {
+      var rowSelected2 = document.getElementById(highlightedRow); 
+      console.log("highlightedRow", highlightedRow);
+      rowSelected2.style.backgroundColor = "rgb(255, 255, 255)";
+      rowSelected2.style.color = "rgb(0, 0, 0)";
+      var data = document.getElementById(highlightedRow).querySelectorAll("td");
+      data[0].innerHTML='<i class="bi bi-pencil-square" onclick="editFunction(event)"></i>';
+      console.log(rowId);
+      var edit = document.getElementById(highlightedRow).querySelectorAll("td");
+      console.log(edit);
+      edit[5].firstElementChild.disabled = true;
 
-        }
+      var returnToPreviousState = document.getElementById(highlightedRow).querySelectorAll("td");
+      console.log(returnToPreviousState);
+      returnToPreviousState[5].firstElementChild.disabled = true;
+
+      console.log(returnToPreviousState);
+      returnToPreviousState[6].contentEditable = false;
+
+      console.log(returnToPreviousState);
+      returnToPreviousState[7].contentEditable = false;
+
     }
-}
 
-function test(event) {
-  var rowId2 = event.target.parentNode.parentNode.id;
-  if(event.target) {
-    a = rowId2
-    console.log(a)
+    highlightedRow = rowId;
+    
   }
+  //5,6,7
+  var edit = document.getElementById(rowId).querySelectorAll("td");
+  console.log(edit);
+  edit[5].firstElementChild.disabled = false;
+
+  console.log(edit);
+  edit[6].contentEditable = true;
+
+  console.log(edit);
+  edit[7].contentEditable = true;
+
+
+}
+ 
+function saveFunction(event){
+  var saveData = document.getElementById(rowId).querySelectorAll("td");
+  console.log(saveData);
+  saveData[5].firstElementChild.value;
+  console.log(saveData[5].firstElementChild.value);
+
+  var venue = saveData[6].innerText;
+  console.log(venue);
+
+  var interviewer = saveData[7].innerText;
+  console.log(interviewer);
+
+  var result = saveData[8].innerText;
+  console.log(result);
+
+  if (highlightedRow != undefined) {
+    var rowSelected2 = document.getElementById(highlightedRow);
+    console.log("highlightedRow", highlightedRow);
+    rowSelected2.style.backgroundColor = "rgb(255, 255, 255)";
+    rowSelected2.style.color = "rgb(0, 0, 0)";
+    var data = document.getElementById(highlightedRow).querySelectorAll("td");
+    data[0].innerHTML='<i class="bi bi-pencil-square" onclick="editFunction(event)"></i>';
+    console.log(rowId);
+    var edit = document.getElementById(highlightedRow).querySelectorAll("td");
+    console.log(edit);
+    edit[5].firstElementChild.disabled = true;
+
+    
+  }
+
+
 }
 
+function cancelFunction(event){
+  var cancelData = document.getElementById(rowId).querySelectorAll("td");
+  cancelData[5].firstElementChild.value = selectedRowData.date;
+  cancelData[6].innerHTML = selectedRowData.venue;
+  cancelData[7].innerHTML = selectedRowData.interviewer;
+  cancelData[8].innerHTML = selectedRowData.result;
 
-function selectedrow(event) {
-    var rowId = event.target.parentNode.id;
-    if ( a == undefined) {
-      rowdata = rowId;
-    }
-    else if ( a != undefined) {
-      rowdata = a;
-    }
-    a = undefined;
-  //this gives id of tr whose button was clicked
-  /*returns array of all elements with 
-  "row-data" class within the row with given id*/
-    console.log(rowdata);
+  if (highlightedRow != undefined) {
+    var rowSelected2 = document.getElementById(highlightedRow);
+    console.log("highlightedRow", highlightedRow);
+    rowSelected2.style.backgroundColor = "rgb(255, 255, 255)";
+    rowSelected2.style.color = "rgb(0, 0, 0)";
+    var data = document.getElementById(highlightedRow).querySelectorAll("td");
+    data[0].innerHTML='<i class="bi bi-pencil-square" onclick="editFunction(event)"></i>';
+    console.log(rowId);
+    var edit = document.getElementById(highlightedRow).querySelectorAll("td");
+    console.log(edit);
+    edit[5].firstElementChild.disabled = true;
+  }
 }
 
 //pass-fail function
 function clickedPassed() {
   //this gives id of tr whose button was clicked
-  var data = document.getElementById(rowdata).querySelectorAll(".row-result");
+  var data = document.getElementById(rowId).querySelectorAll("td");
 
-  data[0].innerHTML = "PASSED";
+  data[8].innerHTML = "PASSED";
 }
 
 function clickedFailed() {
   //this gives id of tr whose button was clicked
-  var data = document.getElementById(rowdata).querySelectorAll(".row-result"); 
-  data[0].innerHTML = "FAILED";
+  var data = document.getElementById(rowId).querySelectorAll("td"); 
+  data[8].innerHTML = "FAILED";
 
 }
 
@@ -118,13 +189,13 @@ function clickedFailed() {
 function tableSearch() {
   let input, filter, table, tr, td, txtValue;
 
-  input = document.getElementById("myinput");
+  input = document.getElementById("searchInput");
   filter = input.value;
   table = document.getElementById("mytable");
   tr = table.getElementsByTagName('tr');
 
   for(let i = 0; i < tr.length; i++){
-    td = tr[i].getElementsByTagName('td')[0]
+    td = tr[i].getElementsByTagName('td')[1]
     if(td) {
       txtValue = td.textContent || td.innerText;
       if(txtValue.indexOf(filter) > -1){
@@ -136,6 +207,7 @@ function tableSearch() {
     }
   }
 }
+
 
 //select course
 var rows = $("#tabledata tr");
