d83cd2856d32d1247a5cbe0f2d4f708475900bfe
cefbok <xcef.1991@gmail.com>
2022-01-07 15:33:35 +0800

    nurse

--- admission/medical/js/nurse-table.js
+++ admission/medical/js/nurse-table.js
@@ -13,6 +13,8 @@ let studentData = [
   {name:'Kilario, Roniel', number:'0195', course:'BSCE', email:'kilario@gmail.com', mresult:'', 
   file:'<input type="file" accept=".jpg, .png" id="fileupload" onclick="test(event)"></input>'},
   {name:'Fetalbo, Dominic', number:'0155', course:'BET-COET', email:'Fetable@gmail.com', mresult:'', 
+  file:'<input type="file" accept=".jpg, .png" id="fileupload" onclick="test(event)"></input>'},
+  {name:'Pastor, Jonathan', number:'0205', course:'BET-PPET', email:'Pastor@gmail.com', mresult:'', 
   file:'<input type="file" accept=".jpg, .png" id="fileupload" onclick="test(event)"></input>'}
 ];
 
@@ -39,7 +41,7 @@ function loadTableData(studentData) {
   tableBody.innerHTML = dataHtml
 }
 
-//row selected
+//highlight selected
 highlight_row();
 function highlight_row() {
     var table = document.getElementById('mytable');
@@ -52,20 +54,18 @@ function highlight_row() {
         cell.onclick = function () {
             // Get the row id where the cell exists
             var rowId = this.parentNode.rowIndex;
-
             var rowsNotSelected = table.getElementsByTagName('tr');
             for (var row = 0; row < rowsNotSelected.length; row++) {
                 rowsNotSelected[row].style.backgroundColor = "";
                 rowsNotSelected[row].classList.remove('selected');
             }
             var rowSelected = table.getElementsByTagName('tr')[rowId];
-           // rowSelected.style.backgroundColor = "rgb(189, 32, 49)";
             rowSelected.className += " selected";
-
         }
     }
 }
 
+//choose file
 function test(event) {
   var rowId2 = event.target.parentNode.parentNode.id;
   if(event.target) {
@@ -74,7 +74,7 @@ function test(event) {
   }
 }
 
-
+//selected row function
 function selectedrow(event) {
     var rowId = event.target.parentNode.id;
     if ( a == undefined) {
@@ -84,9 +84,6 @@ function selectedrow(event) {
       rowdata = a;
     }
     a = undefined;
-  //this gives id of tr whose button was clicked
-  /*returns array of all elements with 
-  "row-data" class within the row with given id*/
     console.log(rowdata);
 }
 
@@ -94,21 +91,17 @@ function selectedrow(event) {
 function clickedPassed() {
   //this gives id of tr whose button was clicked
   var data = document.getElementById(rowdata).querySelectorAll(".row-result");
-
   data[0].innerHTML = "PASSED";
 }
-
 function clickedFailed() {
   //this gives id of tr whose button was clicked
   var data = document.getElementById(rowdata).querySelectorAll(".row-result"); 
   data[0].innerHTML = "FAILED";
-
 }
 
 //search table
 function tableSearch() {
   let input, filter, table, tr, td, txtValue;
-
   input = document.getElementById("myinput");
   filter = input.value;
   table = document.getElementById("mytable");
@@ -130,18 +123,12 @@ function tableSearch() {
 
 //select course
 var rows = $("#tabledata tr");
-
 $("#select-course").on("change", function() {
-
     var selected = this.value;
-
     if (selected != "All") {
-
         rows.filter("[course=" + selected + "]").show();
-        rows.not("[course=" + selected + "]").hide();
-        
+        rows.not("[course=" + selected + "]").hide(); 
     } else {
         rows.show();
     }
-
 });
