e348e93d7a2642f445205e8ae4e21e1a42dcdc61
paopaolopaoloo <simonruzzel@gmail.com>
2022-02-06 16:02:33 +0800

    Adding Models

--- TUPCAdmission/admissionApp/models.py
+++ TUPCAdmission/admissionApp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Applicant(models.Model):
+    # control_number = models.CharField()
+    pass
+
+
+class Coordinator(models.Model):
+    pass
+
+
+class Medical(models.Model):
+    pass
+
+
+class Interviewer(models.Model):
+    pass
+
+
+class Accounts_Admins(models.Model):
+    pass
+
+
