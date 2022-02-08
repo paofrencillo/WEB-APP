26f0f8e8f04eacfcccd5f93bac6f90c1c4e90dc6
cefbok <xcef.1991@gmail.com>
2022-02-08 14:50:06 +0800

    table

--- TUPCAdmission/TUPCAdmission/urls.py
+++ TUPCAdmission/TUPCAdmission/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('', include('admissionApp.urls')),
     path('admin/', admin.site.urls),
     re_path('create_applicant/', views.create_applicant, name="create_applicant"),
+    re_path('medical_table/',views.medical_table, name="medical_table"),
 ]
