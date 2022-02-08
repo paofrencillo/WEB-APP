4113b25aa3a199dd4897e466d91b86d5689bd442
paopaolopaoloo <simonruzzel@gmail.com>
2022-02-08 14:49:02 +0800

    html template

--- TUPCAdmission/admissionApp/models.py
+++ TUPCAdmission/admissionApp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class ApplicantInfo(models.Model):
-    control_number = models.IntegerField(verbose_name="control_number")
+    control_number = models.IntegerField(verbose_name="control_number", unique=True)
     fullName = models.CharField(max_length=255, verbose_name="fullName")
     birthdate  = models.DateField(verbose_name="birthdate")
     sex =  models.CharField(max_length=6, verbose_name="sex")
@@ -15,7 +15,7 @@ class ApplicantInfo(models.Model):
 
 
 class Coordinator(models.Model):
-    control_number = models.IntegerField(verbose_name="control_number")
+    control_number = models.IntegerField(verbose_name="control_number", unique=True)
     score =  models.IntegerField(verbose_name="control_number")
     card = models.BooleanField()
     bc = models.BooleanField()
@@ -25,7 +25,7 @@ class Coordinator(models.Model):
 
 class Medical(models.Model):
     name = models.CharField(max_length=50, verbose_name="name")
-    control_number = models.IntegerField(null=True, verbose_name="control_number")
+    control_number = models.IntegerField(null=True, verbose_name="control_number", unique=True)
     course = models.CharField(max_length=50, verbose_name="course")
     email = models.EmailField(max_length=100, verbose_name="email")
     medical_result = models.CharField(max_length=50, verbose_name="result")
@@ -34,7 +34,7 @@ class Medical(models.Model):
 
 class Interviewer(models.Model):
     name = models.CharField(max_length=50, verbose_name="name")
-    control_number = models.IntegerField(verbose_name="control_number")
+    control_number = models.IntegerField(verbose_name="control_number", unique=True)
     interview_date  = models.DateField(verbose_name="interview_date")
     venue = models.CharField(max_length=50, verbose_name="venue")
     interviewer = models.CharField(max_length=50, verbose_name="interviewer")
@@ -42,6 +42,7 @@ class Interviewer(models.Model):
 
     class Meta:
         db_table = "InterviewerInfoTable"
+
 
 class Accounts_Admins(models.Model):
     account_type = models.CharField(max_length=10, verbose_name="account_type")
--- TUPCAdmission/admissionApp/urls.py
+++ TUPCAdmission/admissionApp/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('coordinator-login/', views.coordinator_login, name='coordinator-login'),
     # Medical Login Page
     path('medical-login/', views.medical_login, name='nurse-login'),
-     # Medical table Page
+    # Medical table Page
     path('medical-table/', views.medical_table, name='nurse-table'),
     # Interview Login Page
     path('interviewer-login/', views.interviewer_login, name='interviewer-login'),
