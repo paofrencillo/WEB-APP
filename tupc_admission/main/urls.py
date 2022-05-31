20e63330a7998a81c7042848a505202f6b3f1662
paopaolopaoloo <simonruzzel@gmail.com>
2022-05-31 12:37:47 +0800

    yeah

--- tupc_admission/main/urls.py
+++ tupc_admission/main/urls.py
@@ -1,5 +1,6 @@
 
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 
@@ -45,4 +46,14 @@ urlpatterns = [
 
     ##### ---------- CREATE ACCOUNTS'S URLS ---------- #####
     path("create_account/TUPCAdmission/", views.create_admissionAccounts, name="create_admissionAccounts"),
+
+    ##### ---------- RESET PASSWORD URLS ---------- #####
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'),
+         name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+         name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
