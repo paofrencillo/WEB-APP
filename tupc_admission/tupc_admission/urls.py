6fadde4c5b23036114e2367e552bc56deccd1eba
paopaolopaoloo <simonruzzel@gmail.com>
2022-07-09 19:58:20 +0800

    FOR DEPLOYMENT

--- tupc_admission/tupc_admission/urls.py
+++ tupc_admission/tupc_admission/urls.py
@@ -16,14 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-# REMOVE THIS WHEN PRODUCTION!!!
-from django.conf import settings
-from django.conf.urls.static import static
-#############################################
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("main.urls")),
     path('', include("django.contrib.auth.urls")),
     path('accounts/', include('django.contrib.auth.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
